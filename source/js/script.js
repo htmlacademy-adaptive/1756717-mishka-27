722f9b7dbe5bd006391e2fdd752d718b97ead46f
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-04-11 11:45:48 +0300

    Стилизует мобильное меню при отключенном js

--- source/js/script.js
+++ source/js/script.js
@@ -2,8 +2,10 @@
 
 const header = document.querySelector('.main-header');
 const navToggle = document.querySelector('.main-header__toggle');
+const hero = document.querySelector('.hero');
 
 header.classList.remove('main-header--no-js');
+hero.classList.remove('hero--no-js');
 
 navToggle.addEventListener('click', () => {
   if (header.classList.contains('main-header--opened')) {
