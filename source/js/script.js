98f08d7083cb447fb3fc719ce4f6f0cf2b4405a2
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-04-18 12:14:58 +0300

    Исправляет замечания по результатам проверки

--- source/js/script.js
+++ source/js/script.js
@@ -5,7 +5,9 @@ const navToggle = document.querySelector('.main-header__toggle');
 const hero = document.querySelector('.hero');
 
 header.classList.remove('main-header--no-js');
-hero.classList.remove('hero--no-js');
+if (hero) {
+  hero.classList.remove('hero--no-js');
+}
 
 navToggle.addEventListener('click', () => {
   if (header.classList.contains('main-header--opened')) {
